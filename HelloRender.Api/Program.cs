e8df8a8070e57204a0b5cfa9f066960207269395
Danilo Meireles <danilomeireles@superdevlab.com>
2024-10-07 18:46:31 -0700

    Update Program.cs

--- HelloRender.Api/Program.cs
+++ HelloRender.Api/Program.cs
@@ -1,9 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,7 +14,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Disable HTTPS redirection in production when running on Render.com
+if (!app.Environment.IsProduction() || Environment.GetEnvironmentVariable("RENDER") != "true")
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
